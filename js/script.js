13998d54810fda38281576e9cc1f5ac0b53b49d5
Arsh Goyal <arshgoyalDev@gmail.com>
2024-06-13 23:43:18 +0530

    feat: mark all read

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// func() => to mark notification read;
+
+const markRead = function (noti) {
+  let readMarker = document.querySelector(`#${noti.id} .unread-marker`);
+
+  noti.classList.remove("unread");
+  readMarker.classList.remove("unread");
+}
+
 // mark notification read
 
 let notiSet = document.querySelectorAll(".notification");
@@ -5,12 +14,22 @@ let notiSet = document.querySelectorAll(".notification");
 notiSet.forEach((noti) => {
   noti.addEventListener("click", (e) => {
     if (noti.classList.contains("unread")) {
-      let readMarker = document.querySelector(`#${noti.id} .unread-marker`);
-
-      noti.classList.remove("unread");
-      readMarker.classList.remove("unread");
-    } else {}
+      markRead(noti);
+    } else {
+      alert("Already Marked");
+      // return;
+    }
   });
 });
 
+// mark all read
 
+let markAllBtn = document.querySelector(".mark-read-btn");
+
+const markAllRead = function () {
+  notiSet.forEach((noti) => {
+    markRead(noti);
+  });
+};
+
+markAllBtn.addEventListener("click", markAllRead);
